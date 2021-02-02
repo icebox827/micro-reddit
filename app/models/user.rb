d76cc53cc4fb12fc25a9f580837b9f1ebdfb365d
icebox827 <icebox827@gmail.com>
2021-02-02 14:51:24 -0500

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 8 }
-  validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_171232) do
-
-  create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "post_id"
+ActiveRecord::Schema.define(version: 20_210_202_171_232) do
+  create_table 'comments', force: :cascade do |t|
+    t.text 'body'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'user_id'
+    t.integer 'post_id'
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.date "date_of_birth"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password'
+    t.date 'date_of_birth'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
