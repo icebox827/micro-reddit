ab4e3958a30306cf9b7e12644cb4482339036cea
icebox827 <icebox827@gmail.com>
2021-02-02 11:36:22 -0500

    Create validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: { case_sensitive: true }, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}, format: { with: ConstantData::VALID_EMAIL_REGEX }
 end
