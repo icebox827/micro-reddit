dcad19ba8d05649e2e795e6e524e1772aafc5e46
Mohammed <atefhamosa@gmail.com>
2021-02-02 20:03:33 +0200

    Fix validation problem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-    belong_to :user
-    belong_to :post
+    belongs_to :user
+    belongs_to :post
 
     validates :body, presence: true, length: {maximum: 250}    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     has_many :comments
-    belong_to :user
-
-    validates :title, presence :true, length: {minimum:3, maximum:15}
-    validates :content, presence :true, length: {minimum:20, maximum:250}
+    belongs_to :user
+    validates_associated :user
+    validates :title, presence: true, length: {minimum:3, maximum:50}
+    validates :content, presence: true, length: {minimum:20, maximum:250}
 end
